f93eddb52a3df33505bdb162d09acceeaa3a6568
richpurba <tepetrol@yahoo.com>
2019-11-30 10:51:54 +0100

    with Socket only

--- src/main/java/com/inter/language/com/Server.java
+++ src/main/java/com/inter/language/com/Server.java
@@ -1,4 +1,30 @@
 package com.inter.language.com;
 
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
 public class Server {
+
+
+    public static void main(String[] args) throws IOException {
+        startingServer("127.0.0.1",80);
+    }
+
+    public static void startingServer(String address, int portNumber) throws IOException {
+
+        try(Socket socket = new Socket()){
+            SocketAddress socketAddress = new InetSocketAddress(InetAddress.getByName(address),portNumber);
+            int timeoutMillis = 2000;
+            long start = System.currentTimeMillis();
+            socket.connect(socketAddress, timeoutMillis);
+            long stop = System.currentTimeMillis();
+            long diff = stop - start;
+            System.out.println("the difference is "+diff);
+        }catch (SocketTimeoutException e){
+            throw new SocketException("Time out"+e);
+        }
+    }
 }
--- src/test/java/com/inter/language/com/ServerTest.java
+++ src/test/java/com/inter/language/com/ServerTest.java
@@ -1,4 +1,16 @@
 package com.inter.language.com;
 
 public class ServerTest {
+    public static void main(String[] args) {
+        Runnable client = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+    }
+
+    public void startClient(){
+        Server serverTest = new Server();
+    }
 }
